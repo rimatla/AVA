4c35557dd5c00381f7fe6b670253c904ed735e71
rimatla <alt.coelho@gmail.com>
2017-10-25 18:33:03 -0500

    refactoring

--- ava_test/app/components/Toggle.test.js
+++ ava_test/app/components/Toggle.test.js
@@ -8,17 +8,30 @@ import {Simulate} from 'react-addons-test-utils'
 
 import Toggle from './Toggle'
 
+// change this from `test.skip(` to simply `test(`
 test('toggle--off class applied by default', t => {
+  // render <Toggle /> with renderToStaticMarkup and get the output
+  // assert the the output string includes the text for the classname
   const output = renderStatic()
   t.true(output.includes('toggle--off'))
 })
 
 test('toggle--on class applied when initialToggledOn specified to true', t => {
+  // render <Toggle /> with renderToStaticMarkup and get the output
+  // assert the the output string includes the text for the classname
   const output = renderStatic({initialToggledOn: true})
   t.true(output.includes('toggle--on'))
 })
 
 test('invokes the onToggle prop when clicked', t => {
+  // create a spy to pass in as the onToggle prop (you'll need to import sinon)
+  // use document.createElement to create a div
+  // render <Toggle /> with your onToggle prop into the div using `render` from `react-dom`
+  // get a reference to the button using `div.querySelector`
+  // Use `Simulate.click` from `react-addons-test-utils` to simulate a click event on the `button`
+  // validate the div's `innerHTML` includes the right class
+  // validate your onToggle spy was called (only once)
+  // validate your onToggle spy was called with the right state (true/false)
   const onToggle = sinon.spy()
   const div = renderToDiv({onToggle})
   const button = div.querySelector('button')
--- ava_test/app/store/Customers.test.js
+++ ava_test/app/store/Customers.test.js
@@ -3,22 +3,35 @@ import sinon from 'sinon'
 import store from './Customers'
 
 test('customers should start with empty', t => {
+  // call store.getCustomers and verify the result is empty
   const customers = store.getCustomers()
   t.true(customers.length === 0)
 })
 
 test('setting customers and getting them', t => {
+  // create two or more objects with a string property called `name`
+  // call store.setCustomers with an array of these objects
+  // call store.getCustomers
+  // validate that what is returned has the proper length
+  // validate that the contents are the same as the contents of the array you passed
   const c0 = {name: 'Bill'}
   const c1 = {name: 'Francine'}
   store.setCustomers([c0, c1])
   const customers = store.getCustomers()
   const [sc0, sc1] = customers
   t.true(customers.length === 2)
-  t.same(c0, sc0)
-  t.same(c1, sc1)
+  t.deepEqual(c0, sc0)
+  t.deepEqual(c1, sc1)
 })
 
 test('subscribing to the store', t => {
+  // create a function spy with `sinon.spy()`
+  // use that spy to subscribe to the store and assign the unsubscribe function
+  // call store.setCustomers
+  // validate that the spy was called once
+  // reset the spy with `spy.reset()`
+  // then call the unsubscribe function
+  // validate that calling store.setCustomers again will not call the spy
   const spy = sinon.spy()
   const unsubscribe = store.subscribe(spy)
   store.setCustomers([])
@@ -29,4 +42,5 @@ test('subscribing to the store', t => {
   t.false(spy.called)
 })
 
+// add an afterEach here to reset the customers to an empty array
 test.afterEach(() => store.setCustomers([]))
